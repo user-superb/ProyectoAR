86951710007419af94d501a404f8d38f3b2a1ec2
Chris <almeidachristian94@hotmail.com>
2025-05-30 11:24:41 -0300

    Script LogicaTactil agregado
    Codigo conectado con XR Origin que maneja la logica de tocar las entradas de las compuertas.

--- Assets/Scripts/LogicaTactil.cs
+++ Assets/Scripts/LogicaTactil.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using UnityEngine.XR.ARFoundation; 
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class LogicaTactil : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public ComportamientoEntradas IN;
     void Update()
     {
-        
+#if UNITY_EDITOR
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector2 mousePosition = Mouse.current.position.ReadValue();
+            ProcesarToque(mousePosition);
+        }
+#else
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+            ProcesarToque(touchPosition);
+        }
+#endif
+    }
+
+    void ProcesarToque(Vector2 posicion)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(posicion);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            IN = hit.collider.GetComponent<ComportamientoEntradas>();
+            if (IN != null)
+            {
+                Debug.Log("Objeto tocado: " + hit.collider.gameObject.name);
+                IN.onTouch();
+            }
+            else
+            {
+                Debug.Log("Objeto tocado no tiene ComportamientoEntradas: " + hit.collider.gameObject.name);
+            }
+        }
+        else
+        {
+            Debug.Log("Raycast no golpeó nada.");
+        }
     }
 }
