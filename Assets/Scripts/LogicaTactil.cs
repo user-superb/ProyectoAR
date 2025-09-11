5385fe2e243c08867b6d5a95c30c24aad1599c52
Chris <almeidachristian94@hotmail.com>
2025-09-11 16:06:15 -0300

    Entradas reconfiguradas
    Estaba comentado todo el script de logica, no wonder it didn't work

--- Assets/Scripts/LogicaTactil.cs
+++ Assets/Scripts/LogicaTactil.cs
@@ -9,42 +9,22 @@ public class LogicaTactil : MonoBehaviour
     public ComportamientoEntradas IN;
     void Update()
     {
-//#if UNITY_EDITOR
-//        if (Mouse.current.leftButton.wasPressedThisFrame)
-//        {
-//            Vector2 mousePosition = Mouse.current.position.ReadValue();
-//            ProcesarToque(mousePosition);
-//        }
-//#else
-//        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-//        {
-//            Vector2 touchPosition = Input.GetTouch(0).position;
-//            ProcesarToque(touchPosition);
-//        }
-//#endif
+    #if UNITY_EDITOR
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector2 mousePosition = Mouse.current.position.ReadValue();
+            ProcesarToque(mousePosition);
+        }
+    #else
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+            ProcesarToque(touchPosition);
+        }
+    #endif
     }
 
-  /*  void ProcesarToque(Vector2 posicion)
-    {
-        Ray ray = Camera.main.ScreenPointToRay(posicion);
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            IN = hit.collider.GetComponent<ComportamientoEntradas>();
-            if (IN != null)
-            {
-                Debug.Log("Objeto tocado: " + hit.collider.gameObject.name);
-                IN.onTouch();
-            }
-            else
-            {
-                Debug.Log("Objeto tocado no tiene ComportamientoEntradas: " + hit.collider.gameObject.name);
-            }
-        }
-        else
-        {
-            Debug.Log("Raycast no golpeó nada.");
-        }
-    }*/
+  
 
     void ProcesarToque(Vector2 posicion)
 {
