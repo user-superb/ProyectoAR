c78ce9afeae95f78e20f454c0accdc7171028fdc
ezequiel <alcayaga147147@gmail.com>
2025-09-13 21:19:13 -0300

    arreglo del error de LogicaTactil

--- Assets/Scripts/LogicaTactil.cs
+++ Assets/Scripts/LogicaTactil.cs
@@ -1,57 +1,60 @@
 using UnityEngine;
-using UnityEngine.XR.ARFoundation; 
+using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using System.Collections.Generic;
+
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 public class LogicaTactil : MonoBehaviour
 {
     public ComportamientoEntradas IN;
+
+    void OnEnable()  => EnhancedTouchSupport.Enable();
+    void OnDisable() => EnhancedTouchSupport.Disable();
+
     void Update()
     {
-    #if UNITY_EDITOR
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+#if UNITY_EDITOR
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
         {
             Vector2 mousePosition = Mouse.current.position.ReadValue();
             ProcesarToque(mousePosition);
         }
-    #else
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+#endif
+        if (Touch.activeTouches.Count > 0)
         {
-            Vector2 touchPosition = Input.GetTouch(0).position;
-            ProcesarToque(touchPosition);
+            var t = Touch.activeTouches[0];
+            if (t.phase == TouchPhase.Began)
+            {
+                ProcesarToque(t.screenPosition);
+            }
         }
-    #endif
     }
 
-  
-
     void ProcesarToque(Vector2 posicion)
-{
-    Ray ray = Camera.main.ScreenPointToRay(posicion);
-
-    // Layer que solo afecta a estos raycast
-    int layerMask = LayerMask.GetMask("CapsuleOnly");
-
-    if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask))
     {
-        IN = hit.collider.GetComponent<ComportamientoEntradas>();
-        if (IN != null)
+        Ray ray = Camera.main.ScreenPointToRay(posicion);
+        int layerMask = LayerMask.GetMask("CapsuleOnly");
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask))
         {
-            Debug.Log("Objeto tocado: " + hit.collider.gameObject.name);
-            IN.onTouch();
+            IN = hit.collider.GetComponent<ComportamientoEntradas>();
+            if (IN != null)
+            {
+                Debug.Log("Objeto tocado: " + hit.collider.gameObject.name);
+                IN.onTouch();
+            }
+            else
+            {
+                Debug.Log("Objeto tocado no tiene ComportamientoEntradas: " + hit.collider.gameObject.name);
+            }
         }
         else
         {
-            Debug.Log("Objeto tocado no tiene ComportamientoEntradas: " + hit.collider.gameObject.name);
+            Debug.Log("Raycast no golpeó nada en la capa CapsuleOnly.");
         }
     }
-    else
-    {
-        Debug.Log("Raycast no golpeó nada en la capa CapsuleOnly.");
-    }
-}
-
-
-
 }
